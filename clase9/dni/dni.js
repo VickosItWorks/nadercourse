9c169355742187159597999a0e871a80d2648bf7
Vicko Solda <vic.soldaor@gmail.com>
2021-08-29 17:30:14 +0200

    comiteando cambios de hace 2 meses

--- clase9/dni/dni.js
+++ clase9/dni/dni.js
@@ -24,12 +24,23 @@ const asignar = [
   "E",
 ];
 
-const dni = prompt("Ingrese su DNI");
+let dni = prompt("Ingrese su DNI");
 
-if (isNaN(dni) || dni > 99999999 || dni <= 0) {
-  document.write("El valor ingresado no es correcto");
-} else {
-  document.write(
-    "La letra que corresponde con su dni es: " + asignar[dni % 23]
-  );
+while (dni != null) {
+  if (isNaN(dni)) {
+    alert("Lo introducido no es un numero");
+  } else if (dni > 99999999 || dni <= 0) {
+    alert("El valor ingresado no es correcto");
+  } else {
+    alert("La letra que corresponde con su dni es: " + asignar[dni % 23]);
+  }
+  dni = prompt("Ingrese su DNI");
 }
+
+// if (isNaN(dni) || dni > 99999999 || dni <= 0) {
+//   document.write("El valor ingresado no es correcto");
+// } else {
+//   document.write(
+//     "La letra que corresponde con su dni es: " + asignar[dni % 23]
+//   );
+// }
